d84e3d76b6ecce1446eab4c351a40d377c68a672
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-04-27 22:19:48 +0200

    Added openid SSO google and profile page

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 function FrontPage() {
   return (
@@ -18,14 +24,116 @@ function FrontPage() {
   );
 }
 
+// TODO: https://accounts.google.com/.well-known/openid-configuration
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+
+  return await res.json();
+}
+
+function Login() {
+  useEffect(async () => {
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+
+    const parameters = {
+      response_type: "token",
+      client_id:
+        "1015959050003-rgbuu41fh7a0q95jg2dtj7ukg613eqsp.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+
+    window.location.href =
+      authorization_endpoint + "?" + new URLSearchParams(parameters);
+  }, []);
+
+  return (
+    <div>
+      <h1>Please wait...</h1>
+    </div>
+  );
+}
+
+function LoginCallback() {
+  const navigate = useNavigate();
+  useEffect(async () => {
+    const { access_token } = Object.fromEntries(
+      new URLSearchParams(window.location.hash.substring(1))
+    );
+    console.log(access_token);
+
+    await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+    navigate("/");
+  });
+
+  return <h1>Please wait...</h1>;
+}
+
+function useLoader(loadingFn) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  async function load() {
+    try {
+      setLoading(true);
+      setData(await loadingFn());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => load(), []);
+  return { loading, data, error };
+}
+
+function Profile() {
+  const { loading, data, error } = useLoader(async () => {
+    return await fetchJSON("/api/login");
+  });
+
+  if (loading) {
+    return <div>Please wait...</div>;
+  }
+  if (error) {
+    return <div>Error! {error.toString()}</div>;
+  }
+
+  return (
+    <div>
+      <h1>
+        Profile for {data.name} ({data.email})
+      </h1>
+      <div>
+        <img src={data.picture} alt={"Profile picture"} />
+      </div>
+    </div>
+  );
+}
+
 function Application() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/login"} element={<h1>Login</h1>} />
-        <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
-        <Route path={"/profile"} element={<h1>Profile</h1>} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/login/callback"} element={<LoginCallback />} />
+        <Route path={"/profile"} element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- server/server.js
+++ server/server.js
@@ -1,10 +1,64 @@
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 const app = express();
 
-app.use(express.static("../client/dist"));
+dotenv.config();
 
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+async function fetchJSON(url, options) {
+  const res = await fetch(url, options);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+  return await res.json();
+}
+
+app.get("/api/login", async (req, res) => {
+  const { access_token } = req.signedCookies;
+
+  const { userinfo_endpoint } = await fetchJSON(
+    "https://accounts.google.com/.well-known/openid-configuration"
+  );
+  const userinfo = await fetchJSON(userinfo_endpoint, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  res.json(userinfo);
+});
+
+app.post("/api/login", (req, res) => {
+  const { access_token } = req.body;
+
+  res.cookie("access_token", access_token, { signed: true });
+  res.sendStatus(200);
+});
+
+app.get("/api/login", async (req, res) => {
+  const { access_token } = req.signedCookies;
+
+  const { userinfo_endpoint } = await fetchJSON(
+    "https://accounts.google.com/.well-known/openid-configuration"
+  );
+
+  const userinfo = await fetchJSON(userinfo_endpoint, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  res.json(userinfo);
+});
+
+app.use(express.static("../client/dist"));
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.path.startsWith("/api")) {
     res.sendFile(path.resolve("../client/dist/index.html"));
